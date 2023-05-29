9a66154e9e369af5f129e083da65095a79807cf9
Asan <assanali.rashidov@gmail.com>
2023-05-29 23:31:46 +0600

    final

--- main.py
+++ main.py
@@ -1,31 +1,57 @@
 import pygame
 import random
 
+gamerules = {
+    'rock': ['scissors', 'lizard'],
+    'paper': ['rock', 'spock'],
+    'scissors': ['paper', 'lizard'],
+    'lizard': ['paper', 'spock'],
+    'spock': ['rock', 'scissors']
+}
+
 
 class rps_Sprites(pygame.sprite.Sprite):
     def __init__(self, choice):
         super().__init__()
         self.choice = choice
-        self.image = pygame.transform.scale(pygame.image.load(f'{choice}.png'), (30, 30))
+        self.image = pygame.transform.scale(pygame.image.load(f'{choice}.png'), (40, 40))
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint(30, screen_width - 30)
-        self.rect.y = random.randint(30, screen_height - 30)
         self.speed_x = random.randint(-3, 3)
         self.speed_y = random.randint(-3, 3)
+        if self.speed_x == 0:
+            self.speed_x += random.choice([-1, 1])
+        elif self.speed_y == 0:
+            self.speed_y += random.choice([-1, 1])
 
     def move(self):
         self.rect = self.rect.move(self.speed_x, self.speed_y)
-        if self.rect.left < 0 or self.rect.right > screen_width:
+        if self.rect.left < 0:
             self.speed_x *= -1
-        if self.rect.top < 0 or self.rect.bottom > screen_height:
+            self.rect.left = 1
+        elif self.rect.right > screen_width:
+            self.speed_x *= -1
+            self.rect.right = screen_width - 1
+        if self.rect.top < 0:
             self.speed_y *= -1
+            self.rect.top = 1
+        elif self.rect.bottom > screen_height:
+            self.speed_y *= -1
+            self.rect.bottom = screen_height - 1
 
     def draw(self):
         screen.blit(self.image, self.rect)
 
+    def spawn_sprite(self, sprites):
+        self.rect.x = random.randint(40, screen_width - 40)
+        self.rect.y = random.randint(40, screen_height - 40)
+        for j in sprites:
+            if pygame.sprite.collide_rect(self, j):
+                self.rect.x = j.rect.x + 40
+                self.rect.y = j.rect.y + 40
+
     def convert(self, new_choice):
         self.choice = new_choice
-        self.image = pygame.transform.scale(pygame.image.load(f'{new_choice}.png'), (30, 30))
+        self.image = pygame.transform.scale(pygame.image.load(f'{new_choice}.png'), (40, 40))
 
 
 def check_collide(sprite1, sprite2):
@@ -34,18 +60,10 @@ def check_collide(sprite1, sprite2):
         sprite1.speed_x, sprite1.speed_y = sprite2.speed_x, sprite2.speed_y
         sprite2.speed_x, sprite2.speed_y = temp_x, temp_y
 
-        if sprite1.choice == 'rock' and sprite2.choice == 'scissors':
-            sprite2.convert('rock')
-        elif sprite1.choice == 'scissors' and sprite2.choice == 'rock':
-            sprite1.convert('rock')
-        elif sprite1.choice == 'paper' and sprite2.choice == 'rock':
-            sprite2.convert('paper')
-        elif sprite1.choice == 'rock' and sprite2.choice == 'paper':
-            sprite1.convert('paper')
-        elif sprite1.choice == 'scissors' and sprite2.choice == 'paper':
-            sprite2.convert('scissors')
-        elif sprite1.choice == 'paper' and sprite2.choice == 'scissors':
-            sprite1.convert('scissors')
+        if sprite1.choice in gamerules[sprite2.choice]:
+            sprite1.convert(sprite2.choice)
+        elif sprite2.choice in gamerules[sprite1.choice]:
+            sprite2.convert(sprite1.choice)
 
 
 pygame.init()
@@ -53,27 +71,41 @@ pygame.mixer.init()
 pygame.mixer.music.load('background_music.mp3')
 pygame.mixer.music.set_volume(0.03)
 pygame.mixer.music.play(-1)
-screen_width = 400
-screen_height = 600
+screen_width = 500
+screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
+background = pygame.image.load('background.jpg')
 clock = pygame.time.Clock()
 
 pieces = pygame.sprite.Group()
-for i in range(random.randrange(2, 8)):
+for i in range(random.randrange(3, 8)):
     new_rock = rps_Sprites('rock')
+    new_rock.spawn_sprite(pieces)
     pieces.add(new_rock)
 
-for i in range(random.randrange(2, 8)):
+for i in range(random.randrange(3, 8)):
     new_paper = rps_Sprites('paper')
+    new_paper.spawn_sprite(pieces)
     pieces.add(new_paper)
 
-for i in range(random.randrange(2, 8)):
+for i in range(random.randrange(3, 8)):
     new_scissors = rps_Sprites('scissors')
+    new_scissors.spawn_sprite(pieces)
     pieces.add(new_scissors)
+
+for i in range(random.randrange(3, 8)):
+    new_lizard = rps_Sprites('lizard')
+    new_lizard.spawn_sprite(pieces)
+    pieces.add(new_lizard)
+
+for i in range(random.randrange(3, 8)):
+    new_spock = rps_Sprites('spock')
+    new_spock.spawn_sprite(pieces)
+    pieces.add(new_spock)
 
 running = True
 while running:
-    screen.fill((255, 229, 229))
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -86,6 +118,6 @@ while running:
                 check_collide(i, j)
 
     clock.tick(30)
-    pygame.display.update()
+    pygame.display.flip()
 
 pygame.quit()
